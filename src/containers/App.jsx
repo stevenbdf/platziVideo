be9933bcc9083b16e0b9ba763a7b0de3d6f8b671
Steven <stevenbdf@gmail.com>
2019-08-30 17:32:32 -0600

    Implements Hooks and FakeAPI

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,39 +4,61 @@ import Search from '../components/Search'
 import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
-import Footer from '../components/Footer'  
+import Footer from '../components/Footer'
+import useInitialState from '../hooks/useInitialState'
 import '../assets/styles/App.scss'
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+const API = 'http://localhost:3000/initalState'
 
-        <Categories title="Mi Lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories title="Originales de Platzi Video">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Footer />
-    </div>
-)
+const App = () => {
 
+    let { mylist, trends, originals } = useInitialState(API)
+    
+    return (
+        <div className="App">
+            <Header />
+            <Search />
+            {
+                mylist &&
+                <Categories title="Mi Lista">
+                    {
+                        mylist.length > 0 ?
+                            mylist.map(item => <CarouselItem key={item.id} {...item} />)
+                            :
+                            <h3 className="empty_list">Sin nada que mostrar :(</h3>
+                    }
+                </Categories>
+            }
+
+            {
+                trends &&
+                <Categories title="Tendencias">
+                    <Carousel>
+                        {
+                            trends.length > 0 ?
+                                trends.map(item => <CarouselItem key={item.id} {...item} />)
+                                :
+                                <h3 className="empty_list">Sin nada que mostrar :(</h3>
+                        }
+                    </Carousel>
+                </Categories>
+            }
+
+            {
+                originals &&
+                <Categories title="Originales de Platzi">
+                    <Carousel>
+                        {
+                            originals.length > 0 ?
+                                originals.map(item => <CarouselItem key={item.id} {...item} />)
+                                :
+                                <h3 className="empty_list">Sin nada que mostrar :(</h3>
+                        }
+                    </Carousel>
+                </Categories>
+            }
+            <Footer />
+        </div>
+    )
+}
 export default App
