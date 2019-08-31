6ba133c3166ea5f447d77dbcad24ea523fc7c3b4
Steven <stevenbdf@gmail.com>
2019-08-31 15:09:42 -0600

    Routes

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
-import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
-import Footer from '../components/Footer'
 import useInitialState from '../hooks/useInitialState'
-import '../assets/styles/App.scss'
+import '../assets/styles/containers/App.scss'
 
 const API = 'http://localhost:3000/initalState'
 
-const App = () => {
+const Home = () => {
 
     let { mylist, trends, originals } = useInitialState(API)
-    
+
     return (
-        <div className="App">
-            <Header />
+        <React.Fragment>
             <Search />
             {
                 mylist &&
@@ -57,8 +54,7 @@ const App = () => {
                     </Carousel>
                 </Categories>
             }
-            <Footer />
-        </div>
+        </React.Fragment>
     )
 }
-export default App
+export default Home
---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../assets/styles/containers/NotFound.scss'
+
+const NotFound = () => (
+    <div className="figure__container">
+        <figure>
+            <div className="sad-mac"></div>
+            <figcaption>
+                <span className="sr-text">Error 404: Not Found</span>
+                <span className="e"></span>
+                <span className="r"></span>
+                <span className="r"></span>
+                <span className="o"></span>
+                <span className="r"></span>
+                <span className="_4"></span>
+                <span className="_0"></span>
+                <span className="_4"></span>
+                <span className="n"></span>
+                <span className="o"></span>
+                <span className="t"></span>
+                <span className="f"></span>
+                <span className="o"></span>
+                <span className="u"></span>
+                <span className="n"></span>
+                <span className="d"></span>
+            </figcaption>
+        </figure>
+    </div>
+)
+
+export default NotFound
